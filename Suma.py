e0604b282e880247d97d89ed9eeb700a65d7bfcf
AlexisRomerr <Alexis.romer70@gmail.com>
2026-03-03 14:06:21 -0600

    se agrega Suma en Python

---
+++ Suma.py
@@ -0,0 +1,31 @@
+# es un comentario
+
+## para declarar una variable
+nombre = "Alexis"
+print(nombre)
+
+edad = 18
+print(nombre, edad, " years old")
+
+## Para textos largos utilizar comillas triples
+
+texto_largo = """aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"""
+print(texto_largo)
+
+numero1 = 10
+numero2 = 20
+Resultado = numero1 + numero2
+
+print("El resultado de la suma es:", Resultado)
+
+Resultado = numero1 - numero2
+
+print("El resultado de la resta es:", Resultado)
+
+Resultado = numero1 / numero2
+
+print("El resultado de la division es:", Resultado)
+
+Resultado = numero1 * numero2
+
+print("El resultado de la multiplicacion es:", Resultado)
