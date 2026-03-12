c54867698d2bd5b29f3488c04d9da85f319ecaf2
AlexisRomerr <Alexis.romer70@gmail.com>
2026-03-12 17:49:03 -0600

    Se agrega un ejemplo para definir las mayusculas y minusculas en python

---
+++ sesion8_Cadenasde_Texto.py
@@ -0,0 +1,85 @@
+#
+poema = ("Siempre intento de mi mente desprenderla",)
+"Cada día más falta me hace, quiero verla",
+"Ya mis nervios no soportan su ausencia",
+"Voy cayendo en un abismo por tenerla",
+"Y me amarra todo el vicio que ella esconde",
+"Al consumirla me está consumiendo a mí",
+"Soy de ella, mas no es mía",
+"Es placer y es agonía",
+"Es antídoto y veneno",
+"Como droga que me mata a fuego lento",
+"Cuerpo y carne, sin nada de sentimiento",
+"Quiere mi cuerpo, solo eso, sin amarme aún",
+"Y yo queriéndole más todo momento",
+"Como droga que me mata a fuego lento",
+"Cuerpo y carne, sin nada de sentimiento",
+"Cada hora crece el vicio por tenerla",
+"Y me olvido que el amor no está en su cuerpo",
+"Como un loco busco formas de dejarla",
+"Pero caigo a sus pies, pobre infeliz",
+"Ya mi vida está cansada de camas de hotel",
+"He fallado al querer obsesionarla",
+"Y me amarra todo el vicio que ella esconde",
+"Al consumirla me está consumiendo a mí",
+"Soy de ella, mas no es mía",
+"Es placer y es agonía",
+
+
+cancion = """ Siempre intento de mi mente desprenderla
+Cada día más falta me hace, quiero verla
+Ya mis nervios no soportan su ausencia
+Voy cayendo en un abismo por tenerla
+Y me amarra todo el vicio que ella esconde
+Al consumirla me está consumiendo a mí
+Soy de ella, mas no es mía
+Es placer y es agonía
+Es antídoto y veneno
+Como droga que me mata a fuego lento
+Cuerpo y carne, sin nada de sentimiento
+Quiere mi cuerpo, solo eso, sin amarme aún
+Y yo queriéndole más todo momento
+Como droga que me mata a fuego lento
+Cuerpo y carne, sin nada de sentimiento
+Cada hora crece el vicio por tenerla
+Y me olvido que el amor no está en su cuerpo
+Como un loco busco formas de dejarla
+Pero caigo a sus pies, pobre infeliz
+Ya mi vida está cansada de camas de hotel
+He fallado al querer obsesionarla
+Y me amarra todo el vicio que ella esconde
+Al consumirla me está consumiendo a mí
+Soy de ella, mas no es mía
+Es placer y es agonía
+Es antídoto y veneno
+Como droga que me mata a fuego lento
+Cuerpo y carne, sin nada de sentimiento
+Quiere mi cuerpo, solo eso, sin amarme aún
+Y yo queriéndole más todo momento
+Como droga que me mata a fuego lento
+Cuerpo y carne sin nada de sentimiento
+Cada hora crece el vicio por tenerla
+Y me olvido que el amor no está en su cuerpo
+Como droga que me mata a fuego lento
+Eres tú mi antídoto y veneno
+De tal manera te deseo que me siento perdido
+Estoy enviciado por tu cuerpo
+No sé si estoy viviendo o muriendo
+Como droga que me mata a fuego lento
+Eres tú mi antídoto y veneno
+Y sé que estoy poniendo en riesgo mi corazón
+Ya que solo tú estás de ocasiones en nuestra relación
+Me matas, pero me gusta
+Como droga que me mata y que deseo
+Eres tú mi antídoto y veneno
+Estoy perdido en tus caprichos
+En la trampa de tus besos
+Eres tú mi antídoto y veneno """
+
+# print(cancion)
+
+cancion_Mayusculas = cancion.upper()
+# print(cancion_Mayusculas)
+
+cancion_Minuscula = cancion.lower()
+print(cancion_Minuscula)
