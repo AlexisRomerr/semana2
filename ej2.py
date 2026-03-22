9f1bd3d4e75db771b82415f1b2a648dc0e633c17
AlexisRomerr <Alexis.romer70@gmail.com>
2026-03-21 22:07:31 -0600

    Se agrega el ejercicio 2  de la tarea en la semana 9 de Python

---
+++ ej2.py
@@ -0,0 +1,8 @@
+frase = input("Ingrese una frase: ")
+print("Frase original:", frase)
+
+frase_mayus = frase.upper()
+print("Frase en mayusculas:", frase_mayus)
+
+frase_minus = frase.lower()
+print("Frase en minusculas:", frase_minus)
