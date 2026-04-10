313a5d7df0e25f2891023d6d893b4f2a59ca3c86
AlexisRomerr <Alexis.romer70@gmail.com>
2026-04-09 21:51:12 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 11: Limpiar espacios, capitalizar palabras y centrar texto con relleno

---
+++ Parcial2_ej11.py
@@ -0,0 +1,8 @@
+cadena = " el nido matinal "
+print("Cadena normal:", cadena)
+
+sin_espacios = cadena.strip().title()
+print("Cadena sin espacios y con la primera mayuscula:", sin_espacios)
+
+centrado = sin_espacios.center(30, "-")
+print("Cadena centrada:", centrado)
