a2150ab1711535355795b95865bed264f15bb9c9
AlexisRomerr <Alexis.romer70@gmail.com>
2026-04-09 21:48:21 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 9: Reemplazar palabras y convertir todo el texto a mayúsculas

---
+++ Parcial2_ej9.py
@@ -0,0 +1,7 @@
+cadena = "Any time y Anytime"
+
+cadena_reemplazo = cadena.replace("Any time", "Always")
+print("Reemplazada:", cadena_reemplazo)
+
+cadena_mayus = cadena_reemplazo.upper()
+print("Cadena en mayusculas:", cadena_mayus)
