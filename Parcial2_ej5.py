f9566e38517ace9bc93d0f70abcb9aa804b8103f
AlexisRomerr <Alexis.romer70@gmail.com>
2026-04-09 21:41:51 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 5: Inversión de mayúsculas/minúsculas y alineación con relleno

---
+++ Parcial2_ej5.py
@@ -0,0 +1,8 @@
+cadena = "pYTHON"
+print("Cadena normal:", cadena)
+
+cadena_invertida = cadena.swapcase()
+print("Cadena invertida mayusculas y minusculas:", cadena_invertida)
+
+cadena_alineada = cadena_invertida.ljust(15, "*")
+print("Cadena alineada:", cadena_alineada)
