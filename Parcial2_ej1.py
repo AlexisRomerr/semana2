27ead9d480072038d19520a977168047e3435b54
AlexisRomerr <Alexis.romer70@gmail.com>
2026-04-09 21:37:06 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 1: Manejo de cadenas, limpieza de espacios y conteo de caracteres

---
+++ Parcial2_ej1.py
@@ -0,0 +1,8 @@
+animal = " elefante "
+print(animal)
+
+animal_sin_espacio = animal.strip()
+print(animal_sin_espacio)
+
+conteo = animal_sin_espacio.count("e")
+print("La letra e se repite:", conteo, "veces")
