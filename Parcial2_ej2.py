7f7931b5bca4fab610bd50c4b8308849c864bc1e
AlexisRomerr <Alexis.romer70@gmail.com>
2026-04-09 21:38:03 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 2: Capitalización de palabras y reemplazo de texto

---
+++ Parcial2_ej2.py
@@ -0,0 +1,8 @@
+nombre = "alexis omar"
+print("nombre normal:", nombre)
+
+nombre_mayus = nombre.title()
+print("nombre con la primera letra en masyucula:", nombre_mayus)
+
+nombre_nuevo = nombre_mayus.replace("Alexis Omar", "García Romero")
+print("Nombre reemplazado:", nombre_nuevo)
