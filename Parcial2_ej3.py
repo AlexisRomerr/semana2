f1af8f20851091b573cb139a8ce564a3322f7644
AlexisRomerr <Alexis.romer70@gmail.com>
2026-04-09 21:39:28 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 3: Remoción de prefijo y conversión a mayúsculas

---
+++ Parcial2_ej3.py
@@ -0,0 +1,8 @@
+nombre_futuro = "ING. Alexis"
+print("Mi nombre cuando sea ingeniero:", nombre_futuro)
+
+nombre_normal = nombre_futuro.removeprefix("ING. ")
+print("Mi nombre normal:", nombre_normal)
+
+nombre_mayus = nombre_normal.upper()
+print("Nombre en mayusculas:", nombre_mayus)
