bfdb7b2be1a55271bd41ee17a5f9d5475742aa16
AlexisRomerr <Alexis.romer70@gmail.com>
2026-04-09 21:42:38 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 6: Normalización casefold y verificación alfabética

---
+++ Parcial2_ej6.py
@@ -0,0 +1,7 @@
+nombre = "Alexis"
+
+nombre_ignorando = nombre.casefold()
+print("Nombre normalizado:", nombre_ignorando)
+
+comprobar = nombre_ignorando.isalpha()
+print("Contiene solo letras?:", comprobar)
