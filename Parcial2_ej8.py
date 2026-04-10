7f6c97909255258b8cae7308d349ed694ae7a776
AlexisRomerr <Alexis.romer70@gmail.com>
2026-04-09 21:47:10 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 8: Crear bloque de texto, contar letras y dividir por líneas

---
+++ Parcial2_ej8.py
@@ -0,0 +1,9 @@
+poema = """Bajo el manto de la noche,
+las estrellas dictan su calma,
+mientras el viento susurra
+"""
+conteo = poema.count("a")
+print("La letra a aparece:", conteo, "veces")
+
+poema_dividido = poema.splitlines()
+print("Poema dividido en lineas", poema_dividido)
