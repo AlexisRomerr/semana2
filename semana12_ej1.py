01eafff9f5895e8578e950e66b1efec426348543
AlexisRomerr <Alexis.romer70@gmail.com>
2026-04-17 17:31:20 -0600

    Semana 12 - Ejercicio 1: Determinar si un número es positivo, negativo o cero usando if/elif/else

---
+++ semana12_ej1.py
@@ -0,0 +1,8 @@
+num = float(input("Ingrese un número: "))
+
+if num > 0:
+    print("Su numero es positivo")
+elif num < 0:
+    print("Su numero es negativo")
+else:
+    print("Su numero es cero")
